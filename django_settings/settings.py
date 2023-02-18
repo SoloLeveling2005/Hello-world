dbe0c768001ca77a46784d13394d3e74d54dec88
Tamirlan Zhukasaev <zukasaevtamirlan1@gmail.com>
2023-02-18 10:40:34 +0600

    corsheaders

--- django_settings/settings.py
+++ django_settings/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
+    'corsheaders',
     'api',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,6 +128,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_URL = 'static/'
 
